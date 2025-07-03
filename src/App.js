e8072aae154ded204a333626ecfef277d7652b9b
Sourav Basak <sourav.new2016@gmail.com>
2025-07-03 06:58:27 +0530

    completed xdisplayname

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFullName(`${event.target[0].value} ${event.target[1].value}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Full Name Display</h1>
+      <div>
+        <label>First Name:</label>
+        <input name="fname" required />
+      </div>
+      <div>
+        <label>Last Name:</label>
+        <input name="lname" required />
+      </div>
+      <button type="submit">Submit</button>
+      {!!fullName?.length && <p>Full Name: {fullName}</p>}
+    </form>
+  );
+}
+
+export default App;
